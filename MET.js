bdb66d40cc188d1ec8e46667e89dafb384c77c27
Bartosz <bgruca17@gmail.com>
2024-09-06 23:26:03 +0200

    chore: Add MET tables for running and cycling

---
+++ MET.js
@@ -0,0 +1,33 @@
+// pace: min/km
+// speed: km/h
+// MET: Metabolic Equivalent of Task
+
+export const metTableForRunning = [
+    { pace: 2, speed: 30, MET: 23.0},
+    { pace: 2.5, speed: 24, MET: 20.0},
+    { pace: 3, speed: 20, MET: 18.0},
+    { pace: 3.5, speed: 17.14, MET: 16.0 },
+    { pace: 4, speed: 15, MET: 15.0 },
+    { pace: 4.5, speed: 13.33, MET: 13.5 },
+    { pace: 5, speed: 12, MET: 12.5 },
+    { pace: 5.5, speed: 10.91, MET: 11.5 },
+    { pace: 6, speed: 10, MET: 10.0 },
+    { pace: 7, speed: 8.57, MET: 9.0 },
+    { pace: 8, speed: 7.5, MET: 8.0 },
+    { pace: 9, speed: 6.67, MET: 7.0 },
+    { pace: 10, speed: 6, MET: 6.0 },
+    { pace: 12, speed: 5, MET: 5.0 }
+];
+
+export const metTableForCycling = [
+    { pace: 1.7, speed: 35.3, MET: 16.0 },
+    { pace: 2, speed: 30, MET: 14.0 },
+    { pace: 2.5, speed: 24, MET: 12.0 },
+    { pace: 3, speed: 20, MET: 10.0 },
+    { pace: 3.5, speed: 17.1, MET: 8.5 },
+    { pace: 4, speed: 15, MET: 8.0 },
+    { pace: 5, speed: 12, MET: 6.8 },
+    { pace: 6, speed: 10, MET: 5.5 },
+    { pace: 7.5, speed: 8, MET: 4.8 },
+    { pace: 10, speed: 6, MET: 4.0 }
+];
